ea07c422effde9c86aec794b9f56a368a14ae0c1
shraddhaL <52315608+shraddhaL@users.noreply.github.com>
2021-01-21 20:47:20 +0530

    Update SearchPage.java

--- src/main/java/com/testautomationguru/container/pages/SearchPage.java
+++ src/main/java/com/testautomationguru/container/pages/SearchPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class SearchPage {
+public class SearchPage throws MalformedURLException{
 
     private final WebDriver driver;
     private final WebDriverWait wait;
